4f087f7eb9b4e7475261bba9f440813464e90499
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-19 03:58:38 +0600

    setup error page

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -3,11 +3,13 @@ import Layouts from "../Layouts/Layouts";
 import Home from "../Pages/Home/Home";
 import SignUp from "../Pages/SignUP/SignUp";
 import Login from "../Pages/Login/Login";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Layouts></Layouts>,
+      errorElement : <ErrorPage></ErrorPage>,
       children : [
         {
             path : '/',
