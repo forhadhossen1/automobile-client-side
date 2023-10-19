6105839b856b0464bcf120e306b141fe67882698
forhadhossen1 <mr.forhad1030@gamil.com>
2023-10-19 17:00:19 +0600

    set card delete system

--- src/Pages/MyCart/MyCart.jsx
+++ src/Pages/MyCart/MyCart.jsx
@@ -8,7 +8,7 @@ const MyCart = () => {
     return (
         <div>
             <h3 className="text-5xl font-bold text-center py-12">My Cart {products.length}</h3>
-            <div className="grid md:grid-cols-2 gap-4">
+            <div className="grid md:grid-cols-2 gap-4 md:container md:mx-auto">
                 {
                     products.map(product => <MyCartCard key={product._id} product={product}></MyCartCard> )
                 }
--- src/Pages/MyCart/MyCartCard.jsx
+++ src/Pages/MyCart/MyCartCard.jsx
@@ -1,32 +1,62 @@
 import Swal from "sweetalert2";
 
-const MyCartCard = ({product}) => {
-    const {_id, name, brand, price, rating, photo} = product || {}
+const MyCartCard = ({ product }) => {
+    const { _id, name, brand, price, rating, photo } = product || {};
+    const handleDelete = _id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be delet this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
 
-   
+
+                fetch(`http://localhost:5000/products/${_id}`,{
+                    method : 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+
+
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl">
-            <figure><img src={photo} alt="car" /></figure>
-            <div className="flex justify-between w-full p-4">
-                <div>
-                    <h2 className="card-title">Name : {name}</h2>
-                    <p>Brand : {brand}</p>
-                    <p>price : {price}</p>
-                    <p>rating : {rating}</p>
-                </div>
-                <div className="card-actions justify-end">
-                    <div className="btn-group btn-group-vertical space-y-3">
-                        <button className="btn btn-active">view</button>
-                        
+                <figure><img src={photo} alt="car" /></figure>
+                <div className="flex justify-between w-full p-4">
+                    <div>
+                        <h2 className="card-title">Name : {name}</h2>
+                        <p>Brand : {brand}</p>
+                        <p>price : {price}</p>
+                        <p>rating : {rating}</p>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <div className="btn-group btn-group-vertical space-y-3">
+                            <button className="btn btn-active">view</button>
+
                             <button className="btn">edit</button>
-                        
-                        <button
-                            className="btn bg-orange-700">X</button>
+
+                            <button onClick={() => handleDelete(_id)}
+                                className="btn bg-orange-700">X</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };
